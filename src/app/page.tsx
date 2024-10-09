43b4523ccd833b2eb69c10b3b0328b8f56f58496
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-10-09 23:35:47 +0300

    New arrivals & top selling sections implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { BrandBanner } from "@/components/BrandBanner/BrandBanner";
 import { Hero } from "@/components/Hero/Hero";
 import { NewArrivals } from "@/components/NewArrivals/NewArrivals";
+import { TopSelling } from "@/components/TopSelling/TopSelling";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<BrandBanner />
+			<TopSelling />
 			<NewArrivals />
 		</>
 	);
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,19 +6,32 @@ import styles from "./ProductCard.module.scss";
 
 export const ProductCard: React.FC<ProductCardProps> = ({ id, thumbnail, title, rating, price, discountPercentage }) => {
 	//Count rating stars
-	let arr = [0];
+	let starCounter = [0];
 	for (let i = 1; i < Math.round(rating); i++) {
-		arr.push(i);
+		starCounter.push(i);
 	}
-
+	//Limit product title
+	const limitTitle = (productTitle: string) => {
+		if (productTitle.length >= 40) {
+			productTitle = productTitle.slice(0, 40).trim() + "...";
+			return productTitle;
+		} else {
+			return productTitle;
+		}
+	};
+	//Handle old price and discount visibility
+	let visibility: string = "block";
+	if (Math.floor(discountPercentage) === 0) {
+		visibility = "none";
+	}
 	return (
-		<Link href={""} className={styles.productCard} title={title}>
+		<Link href={""} className={styles.productCard} title={title} id={String(id)}>
 			<div className={styles.thumbnailWrapper}>
 				<Image src={thumbnail} alt="asd" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
 			</div>
-			<p className={styles.title}>{title}</p>
+			<p className={styles.title}>{limitTitle(title)}</p>
 			<span className={styles.ratingContainer}>
-				{arr.map((num) => {
+				{starCounter.map((num) => {
 					return <Image src={starIcon} alt="star" key={num} />;
 				})}
 				<span className={styles.rating}>
@@ -27,9 +40,15 @@ export const ProductCard: React.FC<ProductCardProps> = ({ id, thumbnail, title, 
 				</span>
 			</span>
 			<div className={styles.priceContainer}>
-				<span className={styles.price}>${Math.round(price - (price * discountPercentage) / 100)}</span>
-				<span className={styles.oldPrice}>${Math.ceil(price)}</span>
-				<span className={styles.discountPercentage}>-%{Math.round(discountPercentage)}</span>
+				<span className={styles.price}>
+					{Math.floor(discountPercentage) !== 0 ? "$" + Math.floor(price - (price * discountPercentage) / 100) : "$" + price}
+				</span>
+				<span className={styles.oldPrice} style={{ display: visibility }}>
+					{"$" + Math.floor(price)}
+				</span>
+				<span className={styles.discountPercentage} style={{ display: visibility }}>
+					{"-%" + Math.round(discountPercentage)}
+				</span>
 			</div>
 		</Link>
 	);
