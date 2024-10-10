2398372177dd872069e183f0b132f775d6d02a67
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-10-10 21:48:01 +0300

    Section implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { BrandBanner } from "@/components/BrandBanner/BrandBanner";
 import { Hero } from "@/components/Hero/Hero";
+import { BrandBanner } from "@/components/BrandBanner/BrandBanner";
+import { BrowseByDressStyle } from "@/components/BrowseByDressStyle/BrowseByDressStyle";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<BrandBanner />
+			<BrowseByDressStyle />
 		</>
 	);
 }
