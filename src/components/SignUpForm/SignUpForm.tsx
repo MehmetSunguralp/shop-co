44672aba2352984b4b2c0a910ee909856012d905
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-11-15 13:54:41 +0300

    SignUp form styling and title update

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -57,64 +57,67 @@ export const SignUpForm = () => {
 				/>
 			</div>
 			<form method="post" className={styles.form} onSubmit={handleSubmit}>
-				<h1 className={styles.title}>SIGN Up FOR A SHOP.CO ACCOUNT</h1>
-				<label htmlFor="username" className={styles.inputLabel}>
-					<Image src={user} alt="lock" className={styles.inputIcons} />
-					<input
-						type="text"
-						id="username"
-						name="username"
-						className={styles.inputArea}
-						placeholder="Enter a username"
-						value={values.username}
-						onChange={handleChange}
-						required
-					/>
-					{errors.username && <p className={styles.error}>{errors.username}</p>}
-				</label>
-				<label htmlFor="email" className={styles.inputLabel}>
-					<Image src={mail} alt="lock" className={styles.inputIcons} />
-					<input
-						type="text"
-						id="email"
-						name="email"
-						className={styles.inputArea}
-						placeholder="Enter your email"
-						value={values.email}
-						onChange={handleChange}
-						required
-					/>
-					{errors.email && <p className={styles.error}>{errors.email}</p>}
-				</label>
-				<label htmlFor="password" className={styles.inputLabel}>
-					<Image src={lock} alt="lock" className={styles.inputIcons} />
-					<input
-						type="password"
-						id="password"
-						name="password"
-						className={styles.inputArea}
-						placeholder="Enter password"
-						value={values.password}
-						onChange={handleChange}
-						required
-					/>
-					{errors.password && <p className={styles.error}>{errors.password}</p>}
-				</label>
-				<label htmlFor="confirm-password" className={styles.inputLabel}>
-					<Image src={lock} alt="lock" className={styles.inputIcons} />
-					<input
-						type="password"
-						id="confirmPassword"
-						name="confirmPassword"
-						className={styles.inputArea}
-						placeholder="Confirm password"
-						value={values.confirmPassword}
-						onChange={handleChange}
-						required
-					/>
-					{errors.confirmPassword && <p className={styles.error}>{errors.confirmPassword}</p>}
-				</label>
-				<input type="submit" value="Sign Up" className={styles.submitBtn} disabled={!isValid || isSubmitting} />
+				<h1 className={styles.title}>CREATE YOUR SHOP.CO ACCOUNT</h1>
+				<div className={styles.inputWrapper}>
+					<label htmlFor="username" className={styles.inputLabel}>
+						<Image src={user} alt="lock" className={styles.inputIcons} />
+						<input
+							type="text"
+							id="username"
+							name="username"
+							className={styles.inputArea}
+							placeholder="Enter a username"
+							value={values.username}
+							onChange={handleChange}
+							required
+						/>
+						{errors.username && <p className={styles.error}>{errors.username}</p>}
+					</label>
+					<label htmlFor="email" className={styles.inputLabel}>
+						<Image src={mail} alt="lock" className={styles.inputIcons} />
+						<input
+							type="text"
+							id="email"
+							name="email"
+							className={styles.inputArea}
+							placeholder="Enter your email"
+							value={values.email}
+							onChange={handleChange}
+							required
+						/>
+						{errors.email && <p className={styles.error}>{errors.email}</p>}
+					</label>
+					<label htmlFor="password" className={styles.inputLabel}>
+						<Image src={lock} alt="lock" className={styles.inputIcons} />
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className={styles.inputArea}
+							placeholder="Enter password"
+							value={values.password}
+							onChange={handleChange}
+							required
+						/>
+						{errors.password && <p className={styles.error}>{errors.password}</p>}
+					</label>
+					<label htmlFor="confirm-password" className={styles.inputLabel}>
+						<Image src={lock} alt="lock" className={styles.inputIcons} />
+						<input
+							type="password"
+							id="confirmPassword"
+							name="confirmPassword"
+							className={styles.inputArea}
+							placeholder="Confirm password"
+							value={values.confirmPassword}
+							onChange={handleChange}
+							required
+						/>
+						{errors.confirmPassword && <p className={styles.error}>{errors.confirmPassword}</p>}
+					</label>
+					<input type="submit" value="Sign Up" className={styles.submitBtn} disabled={!isValid || isSubmitting} />
+				</div>
+
 				<button className={styles.directToLoginBtn} onClick={() => router.push("/login")}>
 					Already have an account? Click here to Log In
 				</button>
