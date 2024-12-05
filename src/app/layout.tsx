24a078a1ddf821980e95f95f86ade98378d919d2
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-12-05 16:19:45 +0300

    Redux setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Providers } from "./providers";
 import { SignUpWarner } from "@/components/SignUpWarner/SignUpWarner";
 import { Header } from "@/components/Header/Header";
 import { Footer } from "@/components/Footer/Footer";
@@ -17,10 +18,12 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={styles.body}>
-				<SignUpWarner />
-				<Header />
-				{children}
-				<Footer />
+				<Providers>
+					<SignUpWarner />
+					<Header />
+					{children}
+					<Footer />
+				</Providers>
 			</body>
 		</html>
 	);
