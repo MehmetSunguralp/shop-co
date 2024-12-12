ff86f4933549136773b6a2309a3ef89e9c14e8b6
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-12-12 12:44:27 +0300

    Signup Form responsiveness schema update

--- src/schemas/SignUpFormSchema.ts
+++ src/schemas/SignUpFormSchema.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const SignUpFormSchema = yup.object().shape({
-	username: yup.string().min(5, "Username must be at least 5 characters!").required("Username is required!"),
-	email: yup.string().email("Unvalid email!").required("Email is required!"),
+	username: yup.string().trim().min(5, "Username must be at least 5 characters!").required("Username is required!"),
+	email: yup.string().trim().email("Unvalid email!").required("Email is required!"),
 	password: yup.string().min(8, "Password must be at least 8 characters!").required("Password is required"),
 	confirmPassword: yup
 		.string()
